99bcb02d8985f711e9e85edccc05456618aa44ca
PuntitOwO <christopher.marin@ug.uchile.cl>
2022-04-02 13:37:26 -0300

    mensajes predeterminados movidos a default_messages

---
+++ default_messages.py
@@ -0,0 +1,3 @@
+START = "Hola! \nSi tienes una pregunta para el CaDCC envía un mensaje con el comando /pregunta@cadccbot seguido de tu pregunta"
+HELP = "Si tienes una pregunta para el CaDCC envía un mensaje con el comando /pregunta@cadccbot seguido de tu pregunta"
+NO_QUESTION = "Ese mensaje no era una pregunta"
